3681aca11d20cfb2c9dbdcdcbcde560d318f7818
Jonny Vorn Soth <janysoth@gmail.com>
2025-02-20 02:16:12 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import fs from "node:fs";
-
 import connect from "./src/db/connect.js";
+import errorHandler from "./src/helpers/errorhandler.js";
 
 dotenv.config();
 
@@ -12,20 +12,25 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
-// Middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
+// middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Routes
+// error handler middleware
+app.use(errorHandler);
+
+//routes
 const routeFiles = fs.readdirSync("./src/routes");
 
-// Use dynamic import
 routeFiles.forEach((file) => {
+  // use dynamic import
   import(`./src/routes/${file}`)
     .then((route) => {
       app.use("/api/v1", route.default);
@@ -43,7 +48,7 @@ const server = async () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
-    console.log("Failed to start server...", error.message);
+    console.log("Failed to strt server.....", error.message);
     process.exit(1);
   }
 };
