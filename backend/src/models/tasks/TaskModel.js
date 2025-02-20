e256258b501a3b594f903b5d59f148ce504df304
Jonny Vorn Soth <janysoth@gmail.com>
2025-02-20 03:15:07 -0600

    `Added mongoose import and commented out unique index creation`

--- backend/src/models/tasks/TaskModel.js
+++ backend/src/models/tasks/TaskModel.js
@@ -1,8 +1,11 @@
+import mongoose from "mongoose";
+
 const TaskSchema = new mongoose.Schema(
   {
     title: {
       type: String,
       required: [true, "Please provide a title"],
+      //unique: true,
     },
 
     description: {
@@ -40,9 +43,6 @@ const TaskSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-
-// Add a compound index for the unique combination of title and user
-TaskSchema.index({ title: 1, user: 1 }, { unique: true });
 
 const Task = mongoose.model("Task", TaskSchema);
 
